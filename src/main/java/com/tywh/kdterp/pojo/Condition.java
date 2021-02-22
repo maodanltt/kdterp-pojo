8b5d114e67cc65d88e489428f363d701be373f6c
maodanltt <maodanltt@163.com>
2021-02-22 23:11:52 +0800

    更新pojo

--- src/main/java/com/tywh/kdterp/pojo/Condition.java
+++ src/main/java/com/tywh/kdterp/pojo/Condition.java
@@ -1,28 +1,10 @@
 package com.tywh.kdterp.pojo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Condition {
-
-    private String bjbmmc;
-
-    private String xsbmmc;
-
-    private String tsfljc;
-
-    private String startdate;
-
-    private String enddate;
-
-    private final String[] fhzts = {"待发","已发"};
-
-    private final String[] tsfljcRemove = {"'%联考%'","'日历%'","'赠品%'","'宣传品%'"};
-
-    private final String khbhRemove = "2000000747";
-
-    private final String sxhRemove = "'W%'";
-
-    private final String kwbh = "00HG";
-
-    private final String kjqj = "2020-01";
 
     public String getBjbmmc() {
         return bjbmmc;
@@ -64,27 +46,75 @@ public class Condition {
         this.enddate = enddate;
     }
 
-    public String[] getFhzts() {
+    public List<String> getFhzts() {
         return fhzts;
     }
 
-    public String[] getTsfljcRemove() {
+    public void setFhzts(List<String> fhzts) {
+        this.fhzts = fhzts;
+    }
+
+    public List<String> getTsfljcRemove() {
         return tsfljcRemove;
+    }
+
+    public void setTsfljcRemove(List<String> tsfljcRemove) {
+        this.tsfljcRemove = tsfljcRemove;
     }
 
     public String getKhbhRemove() {
         return khbhRemove;
     }
 
+    public void setKhbhRemove(String khbhRemove) {
+        this.khbhRemove = khbhRemove;
+    }
+
     public String getSxhRemove() {
         return sxhRemove;
+    }
+
+    public void setSxhRemove(String sxhRemove) {
+        this.sxhRemove = sxhRemove;
     }
 
     public String getKwbh() {
         return kwbh;
     }
 
+    public void setKwbh(String kwbh) {
+        this.kwbh = kwbh;
+    }
+
     public String getKjqj() {
         return kjqj;
     }
+
+    public void setKjqj(String kjqj) {
+        this.kjqj = kjqj;
+    }
+
+    private String bjbmmc;
+
+    private String xsbmmc;
+
+    private String tsfljc;
+
+    private String startdate;
+
+    private String enddate;
+
+    private List<String> fhzts = Arrays.asList("待发","已发");
+
+    private List<String> tsfljcRemove = Arrays.asList("'%联考%'","'日历%'","'赠品%'","'宣传品%'");
+
+    private String khbhRemove = "2000000747";
+
+    private String sxhRemove = "'W%'";
+
+    private String kwbh = "00HG";
+
+    private String kjqj = "2020-01";
+
+
 }
